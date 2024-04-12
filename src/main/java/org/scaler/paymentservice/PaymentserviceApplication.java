5249927af53d0af7ee613c8154ed85fc4309357c
Sparsh Raj <sparsh.raj30@gmail.com>
2024-04-12 11:42:26 +0530

    2nd commit

--- src/main/java/org/scaler/paymentservice/PaymentserviceApplication.java
+++ src/main/java/org/scaler/paymentservice/PaymentserviceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PaymentserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaymentserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentserviceApplication.class, args);
+    }
 
 }
